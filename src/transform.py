fc040959e6f08d7d4583ee80ef3ee0ce0fa9d807
Charlie Timings <charlietimings@Charlies-MacBook-Air.local>
2025-03-23 15:43:04 +0000

    refactor transform code

--- src/transform.py
+++ src/transform.py
@@ -1,16 +1,24 @@
 import pandas as pd
 
-def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+"""
+The transform step comprises two functions: filter_data() and aggregate_data()
+This is to make testing easier
+"""
+
+def filter_data(df: pd.DataFrame) -> pd.DataFrame:
     '''
-    outputs the three most influential articles
+    filters data to only include articles that were read before the user registerd
     args:
         df (dataframe): pandas dataframe containing the imported data from the hitlog
     returns:
-        top_three_articles (dataframe): pandas dataframe containing the three articles that appear most often in all customer journeys
+        filtered_df (dataframe): pandas dataframe containing articles that were read before user registration
     '''
+     
+    # filter out records where any of the fields is null (assume this is bad data so can be ignored)
+    df = df.dropna()
 
     # identify the first registration for each user
-    registration_date = df[df['page_url'].str.contains(r'/registration')].groupby('user_id')['timestamp'].min().reset_index()
+    registration_date = df[df['page_url'].str.contains(r'/registration$')].groupby('user_id')['timestamp'].min().reset_index()
     registration_date = registration_date.rename(columns={'timestamp': 'first_registration_timestamp'})
 
     # left join with df on user_id
@@ -26,10 +34,24 @@ def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     # only count an article once if the user visits the page multiple times
     filtered_df = filtered_df[['page_name', 'page_url', 'user_id']].drop_duplicates()
 
+    return filtered_df
+    
+    
+def aggregate_data(df: pd.DataFrame) -> pd.DataFrame:
+    '''
+    selects the top 3 most influential articles
+    args:
+        df (dataframe): pandas dataframe containing filtered data  
+    returns:
+        top_three_articles (dataframe): pandas dataframe containing the three articles that appear most often in all customer journeys
+    '''
+    
     # count number of times article appeared in all customer journeys
-    article_counts_df = filtered_df.groupby(['page_name', 'page_url']).size().reset_index(name='total')
+    article_counts_df = df.groupby(['page_name', 'page_url']).size().reset_index(name='total')
 
     # order by total and select the top 3 articles
-    top_three_articles = article_counts_df.nlargest(3, 'total').reset_index(drop=True)
+    # if there are multiple articles with the third highest total, show multiple articles
+    top_three_articles = article_counts_df.nlargest(3, 'total', keep='all').reset_index(drop=True)
 
     return top_three_articles
+
--- tests.py
+++ tests.py
@@ -1,7 +1,7 @@
 import pytest
 import pandas as pd
 from src.extract import extract_data
-from src.transform import transform_data
+from src.transform import filter_data, aggregate_data
 
 """
 tests for extracted data from hitlog file
@@ -27,19 +27,41 @@ def test_timestamps(extracted_data):
 
 
 
+
 """
 tests for transform code
 """
 
-pytest.fixture()
-def transformed_data():
-    extracted_data = extract_data('hitlog.csv')
-    transformed_data = transform_data(extracted_data)
-    return transformed_data
-
-def test_transformed_data(transformed_data):
-    # check output type
-    assert isinstance(transformed_data, pd.DataFrame), "Output is not a pandas dataframe"
+# ensure that an article is only counted once if a user loads the same url multiple times
+def test_remove_article_duplicates():
+    # sample data
+    dummy_data = pd.DataFrame(
+        {
+        'page_name': ['article1', 'article2', 'article1', 'registration'],
+        'page_url': ['https://www.telegraph.co.uk/articles/article1', 'https://www.telegraph.co.uk/articles/article2',
+                    'https://www.telegraph.co.uk/articles/article1', 'https://www.telegraph.co.uk/articles/registration'],
+        'user_id': [1234, 1234, 1234, 1234],
+        'timestamp': ['2024-03-22 10:22:17', '2024-03-22 10:23:18', '2024-03-22 10:24:19', 
+                    '2024-03-22 10:36:22']
+        }
+    )
     
-    # confirm only three columns
-    assert len(transformed_data.columns) == 3, "Number of columns is not as expected"
+    # ensure that the filtered df returns two records - article1 and article2 (timestamp is no longer relevant)
+    filtered_data = filter_data(dummy_data)
+    assert len(filtered_data) == 2
+
+
+# check that if an article includes regristration in its name it is still counted
+def test_registration_in_page_url():
+    # sample data
+    dummy_data = pd.DataFrame(
+        {
+        'page_name': ['registration numbers decline at top universities', 'registration'],
+        'page_url': ['https://www.telegraph.co.uk//articles/registration-numbers-decline-at-top-universities', 'https://www.telegraph.co.uk/articles/registration'],
+        'user_id': [1234, 1234],
+        'timestamp': ['2024-03-22 10:22:17', '2024-03-22 10:23:18']
+        }
+    )
+    
+    filtered_data = filter_data(dummy_data)
+    assert filtered_data['page_name'][0] == 'registration numbers decline at top universities'
